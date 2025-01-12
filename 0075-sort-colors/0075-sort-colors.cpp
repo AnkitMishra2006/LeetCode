5e5289504247fbfae7b561d83dcb88a02f200eca
Ankit Mishra <ankit.kumar.mishra2006@gmail.com>
2025-01-12 22:02:49 +0530

    Time: 0 ms (100%), Space: 11.6 MB (24.84%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        return sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int count0 = 0, count1 = 0, count2 = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] == 0) count0++;
+            else if(nums[i] == 1) count1++;
+            else if(nums[i] == 2) count2++;
+        }
+        for(int i = 0; i < n; i++)
+        {
+            if(i < count0) nums[i] = 0;
+            else if(i < count0 + count1) nums[i] = 1;
+            else nums[i] = 2;
+        }
+        return;
     }
 };
