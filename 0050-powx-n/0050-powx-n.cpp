078328bb4baa62088711b70b5270d8398fbb2049
Ankit Kumar Mishra <ankit.kumar.mishra2006@gmail.com>
2025-05-25 01:43:10 +0530

    Time: 0 ms (100%), Space: 8.6 MB (71.36%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     double myPow(double x, int n) {
+
+        if(n == 0 ) return 1.0;
+        if(x == 0) return 0.0;
+        if(x == 1) return 1.0;
+        if(x == -1 && n%2 == 0) return 1.0;
+        if (x == -1 && n%2 != 0) return -1.0;
+
+
+
         double res = 1;
         long binForm = n;
         if(binForm<0)
