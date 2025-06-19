b80ed7589d183aa49a4f3e83c719551b2f24149e
Ankit Kumar Mishra <ankit.kumar.mishra2006@gmail.com>
2025-06-19 16:24:22 +0530

    Time: 0 ms (100%), Space: 10.4 MB (85.63%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+    void getAllSubsets(vector<int>& arr, vector<int>& subset, int index,
+                       vector<vector<int>> &powerSet) {
+        if (index == arr.size()) {
+            powerSet.push_back({subset});
+            return;
+        }
+
+        // Include the current element
+        subset.push_back(arr[index]);
+        getAllSubsets(arr, subset, index + 1, powerSet);
+
+        // Exclude the current element
+        subset.pop_back();
+        int idx = index+1;
+        while(idx < arr.size() && arr[idx] == arr[idx-1]) idx++;
+        getAllSubsets(arr, subset, idx, powerSet);
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        vector<vector<int>> powerSet;
+        vector<int> subset;
+
+        getAllSubsets(nums, subset, 0, powerSet);
+
+        return powerSet;
+    }
+};
